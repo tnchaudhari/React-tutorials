300a692b87ac60983ef731237fcc9951f4d21a6d
tnchaudhari <tnchaudhari0@gmail.com>
2025-07-13 00:17:02 +0530

    Used use Reducer state management in ToDo Item project.

--- context-api/src/App.jsx
+++ context-api/src/App.jsx
@@ -3,35 +3,18 @@ import AddToDo from "./components/AddToDo"
 import ToDoItems from "./components/ToDoItems";
 import WelcomeMessage from "./components/WelcomeMessage";
 import './App.css';
-import { useState } from "react";
-import { ToDoItemsContext } from "./store/todo-items-store";
+import { ToDoItemsContextProvider } from "./store/todo-items-store"
+
 
 function App() {
-  const [toDoItems, setToDoItems] = useState([]);
-
-  const addNewItem = (name, date) => {
-    setToDoItems((currValue) => [
-      ...currValue, { name: name, date: date },
-    ]);
-  }
-
-  const deleteItem = (todoName) => {
-    const newToDoItem = toDoItems.filter(item => item.name != todoName);
-    setToDoItems(newToDoItem);
-  }
-
   return <center className="todo-container">
-    <ToDoItemsContext.Provider value={{
-      toDoItems,
-      addNewItem,
-      deleteItem,
-    }}>
+    <ToDoItemsContextProvider>
       <AppName />
       <AddToDo />
       <WelcomeMessage></WelcomeMessage>
       <ToDoItems></ToDoItems>
-    </ToDoItemsContext.Provider>
-  </center>
+    </ToDoItemsContextProvider>
+  </center >
 }
 
 export default App
--- context-api/src/store/todo-items-store.jsx
+++ context-api/src/store/todo-items-store.jsx
@@ -1,7 +1,60 @@
 import { createContext } from "react";
+import { useReducer } from "react";
 
 export const ToDoItemsContext = createContext({
   toDoItems: [],
   addNewItem: () => { },
   deleteItem: () => { },
 });
+
+const toDoItemsReducer = (currToDoItems, action) => {
+  let newToDoItems = currToDoItems;
+  if (action.type === "NEW_ITEM") {
+    newToDoItems = [...currToDoItems, { name: action.payload.name, date: action.payload.date }];
+
+  } else if (action.type === "DELETE_ITEM") {
+    newToDoItems = newToDoItems.filter(item => item.name != action.payload.name);
+  }
+  return newToDoItems;
+}
+
+export const ToDoItemsContextProvider = ({ children }) => {
+  // const [toDoItems, setToDoItems] = useState([]);
+
+  //State management using use Reducer
+  const [toDoItems, dispatchToDoItems] = useReducer(toDoItemsReducer, []);
+
+  const addNewItem = (name, date) => {
+    // setToDoItems((currValue) => [
+    //   ...currValue, { name: name, date: date },
+    // ]);
+
+    const newToDoItem = {
+      type: "NEW_ITEM",
+      payload: {
+        name,
+        date
+      }
+    }
+    dispatchToDoItems(newToDoItem);
+  }
+
+  const deleteItem = (todoName) => {
+    // const newToDoItem = toDoItems.filter(item => item.name != todoName);
+    // setToDoItems(newToDoItem);
+
+    const deleteToDoItem = {
+      type: "DELETE_ITEM",
+      payload: {
+        name: todoName
+      }
+    }
+    dispatchToDoItems(deleteToDoItem);
+  }
+
+  return <ToDoItemsContext.Provider value={{
+    toDoItems,
+    addNewItem,
+    deleteItem,
+  }}>{children}</ToDoItemsContext.Provider>
+}
